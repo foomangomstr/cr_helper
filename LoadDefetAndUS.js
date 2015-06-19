727f30503161a89af9eba47f8c1b68cd438561dd
Ferry Wang <foomango@gmail.com>
2015-06-19 18:16:28 +0800

    Fix some bugs

--- LoadDefetAndUS.js
+++ LoadDefetAndUS.js
@@ -1,7 +1,20 @@
-var defectAndUS = [];
-$("#x4-gen1518 table .formatted-id-link").each(function (index) {
-    console.log(index + ": " + $(this).text());
-    defectAndUS.push($(this).text());
+var defectAndUS = [],
+    milestoneFilter = ['Usher 2.4.020'];
+$("table tr").each(function (index) {
+    var id = $(this).find('.formatted-id-link').text(),
+        milestones = $(this).find('.milestone-pill'),
+        milestone = null;
+    for (var i = 0; i < milestones.length; i++) {
+        var temp = $(milestones[i]).text();
+        if (milestoneFilter.indexOf(temp) > -1) {
+            milestone = temp;
+            break;
+        }
+    }
+    if (id && milestone && milestone !== null && defectAndUS.indexOf(id) === -1) {
+        console.log(index + ": " + id + "," + milestone);
+        defectAndUS.push(id);
+    }
 });
 defectAndUS.sort();
 defectAndUS;
